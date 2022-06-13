4f2751469b3e923c95f81de3dbb76a9d7e769b23
Kevin Trieu <kevin.trieu5813@gmail.com>
2022-06-13 12:13:27 -0400

    Get ping functionality basically working

--- bot.py
+++ bot.py
@@ -1,12 +1,31 @@
 from dotenv import load_dotenv
+import discord
 
 import argparse
+import os
 import sys
+import asyncio
 
-def cmd_ping():
-    print("Pinging...")
+GENERAL_CHANNEL_ID = '822531930384891948'
 
-def cmd_check():
+MESSAGE = 'UGO-BOT TEST: ALL EMPLOYEES IGNORE'
+
+EMOTE_LIST = ['🍆', '❓', '🛏️', '🙆🏻‍♀️', '💩', '🍠']
+
+async def cmd_ping(client: discord.Client):
+    gen_chan = await client.fetch_channel(GENERAL_CHANNEL_ID)
+    if gen_chan is None:
+        raise RuntimeError('Could not find general channel.')
+
+    message: discord.Message = await gen_chan.send(MESSAGE)
+
+    for emote in EMOTE_LIST:
+        await message.add_reaction(emote)
+
+    await client.close()
+
+
+def cmd_check(client: discord.Client):
     print("Checking...")
 
 parser = argparse.ArgumentParser()
@@ -17,10 +36,22 @@ ping.set_defaults(command=cmd_ping)
 check = subparsers.add_parser('check')
 check.set_defaults(command=cmd_check)
 
-if __name__ == '__main__':
+async def start():
     load_dotenv()
     args = parser.parse_args()
 
-    args.command()
+    intents = discord.Intents()
+    intents.guilds = True
+    intents.messages = True
+    client = discord.Client(intents=intents)
+    token = os.environ.get('DISCORD_TOKEN', None)
+    if not token:
+        print('No bot token found. Provide one via the DISCORD_TOKEN environment variable.')
+        sys.exit(1)
+    await client.login(token)
 
+    await args.command(client)
+
+if __name__ == '__main__':
+    asyncio.run(start())
 
